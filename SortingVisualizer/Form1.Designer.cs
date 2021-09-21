bad7d6b9532fe4a7dee333307ed0640a7f200a74
ValenGamerXD <valentinodidio@hotmail.com>
2021-09-20 22:25:59 -0300

    Agregar archivos de proyecto.

---
+++ SortingVisualizer/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿
+namespace SortingVisualizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.panelBotones = new System.Windows.Forms.Panel();
+            this.panelSuperior = new System.Windows.Forms.Panel();
+            this.panelChild = new System.Windows.Forms.Panel();
+            this.btnBubble = new RJCodeAdvance.RJControls.RJButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.btnInsert = new RJCodeAdvance.RJControls.RJButton();
+            this.btnSelect = new RJCodeAdvance.RJControls.RJButton();
+            this.btnMerge = new RJCodeAdvance.RJControls.RJButton();
+            this.iconPictureBox1 = new FontAwesome.Sharp.IconPictureBox();
+            this.iconPictureBox2 = new FontAwesome.Sharp.IconPictureBox();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.panelBotones.SuspendLayout();
+            this.panelSuperior.SuspendLayout();
+            this.panelChild.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelBotones
+            // 
+            this.panelBotones.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(101)))), ((int)(((byte)(102)))));
+            this.panelBotones.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelBotones.Controls.Add(this.labelStatus);
+            this.panelBotones.Controls.Add(this.btnMerge);
+            this.panelBotones.Controls.Add(this.btnSelect);
+            this.panelBotones.Controls.Add(this.btnInsert);
+            this.panelBotones.Controls.Add(this.btnBubble);
+            this.panelBotones.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelBotones.Location = new System.Drawing.Point(0, 40);
+            this.panelBotones.Name = "panelBotones";
+            this.panelBotones.Size = new System.Drawing.Size(200, 410);
+            this.panelBotones.TabIndex = 0;
+            // 
+            // panelSuperior
+            // 
+            this.panelSuperior.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(115)))), ((int)(((byte)(156)))), ((int)(((byte)(158)))));
+            this.panelSuperior.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelSuperior.Controls.Add(this.iconPictureBox2);
+            this.panelSuperior.Controls.Add(this.iconPictureBox1);
+            this.panelSuperior.Controls.Add(this.label1);
+            this.panelSuperior.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelSuperior.Location = new System.Drawing.Point(0, 0);
+            this.panelSuperior.Name = "panelSuperior";
+            this.panelSuperior.Size = new System.Drawing.Size(800, 40);
+            this.panelSuperior.TabIndex = 1;
+            this.panelSuperior.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelSuperior_MouseDown_1);
+            // 
+            // panelChild
+            // 
+            this.panelChild.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(94)))), ((int)(((byte)(97)))));
+            this.panelChild.Controls.Add(this.chart1);
+            this.panelChild.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelChild.Location = new System.Drawing.Point(200, 40);
+            this.panelChild.Name = "panelChild";
+            this.panelChild.Size = new System.Drawing.Size(600, 410);
+            this.panelChild.TabIndex = 2;
+            // 
+            // btnBubble
+            // 
+            this.btnBubble.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(101)))), ((int)(((byte)(102)))));
+            this.btnBubble.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(101)))), ((int)(((byte)(102)))));
+            this.btnBubble.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(115)))), ((int)(((byte)(156)))), ((int)(((byte)(158)))));
+            this.btnBubble.BorderRadius = 0;
+            this.btnBubble.BorderSize = 1;
+            this.btnBubble.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnBubble.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnBubble.FlatAppearance.BorderSize = 0;
+            this.btnBubble.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBubble.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBubble.ForeColor = System.Drawing.Color.White;
+            this.btnBubble.Location = new System.Drawing.Point(0, 0);
+            this.btnBubble.Name = "btnBubble";
+            this.btnBubble.Size = new System.Drawing.Size(198, 50);
+            this.btnBubble.TabIndex = 0;
+            this.btnBubble.Text = "Burbuja";
+            this.btnBubble.TextColor = System.Drawing.Color.White;
+            this.btnBubble.UseVisualStyleBackColor = false;
+            this.btnBubble.Click += new System.EventHandler(this.btnBubble_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(3, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(246, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Visualizador de Ordenamiento";
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.Color.Transparent;
+            this.chart1.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.DiagonalLeft;
+            this.chart1.BorderlineColor = System.Drawing.Color.Transparent;
+            this.chart1.BorderlineWidth = 0;
+            this.chart1.BorderSkin.BackColor = System.Drawing.Color.Honeydew;
+            this.chart1.BorderSkin.PageColor = System.Drawing.Color.Transparent;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(0, 0);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.SeaGreen;
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(600, 410);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(101)))), ((int)(((byte)(102)))));
+            this.btnInsert.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(101)))), ((int)(((byte)(102)))));
+            this.btnInsert.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(115)))), ((int)(((byte)(156)))), ((int)(((byte)(158)))));
+            this.btnInsert.BorderRadius = 0;
+            this.btnInsert.BorderSize = 1;
+            this.btnInsert.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnInsert.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnInsert.FlatAppearance.BorderSize = 0;
+            this.btnInsert.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInsert.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInsert.ForeColor = System.Drawing.Color.White;
+            this.btnInsert.Location = new System.Drawing.Point(0, 50);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(198, 50);
+            this.btnInsert.TabIndex = 1;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.TextColor = System.Drawing.Color.White;
+            this.btnInsert.UseVisualStyleBackColor = false;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(101)))), ((int)(((byte)(102)))));
+            this.btnSelect.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(101)))), ((int)(((byte)(102)))));
+            this.btnSelect.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(115)))), ((int)(((byte)(156)))), ((int)(((byte)(158)))));
+            this.btnSelect.BorderRadius = 0;
+            this.btnSelect.BorderSize = 1;
+            this.btnSelect.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSelect.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnSelect.FlatAppearance.BorderSize = 0;
+            this.btnSelect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSelect.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSelect.ForeColor = System.Drawing.Color.White;
+            this.btnSelect.Location = new System.Drawing.Point(0, 100);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(198, 50);
+            this.btnSelect.TabIndex = 2;
+            this.btnSelect.Text = "Select";
+            this.btnSelect.TextColor = System.Drawing.Color.White;
+            this.btnSelect.UseVisualStyleBackColor = false;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // btnMerge
+            // 
+            this.btnMerge.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(101)))), ((int)(((byte)(102)))));
+            this.btnMerge.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(101)))), ((int)(((byte)(102)))));
+            this.btnMerge.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(115)))), ((int)(((byte)(156)))), ((int)(((byte)(158)))));
+            this.btnMerge.BorderRadius = 0;
+            this.btnMerge.BorderSize = 1;
+            this.btnMerge.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnMerge.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnMerge.FlatAppearance.BorderSize = 0;
+            this.btnMerge.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMerge.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMerge.ForeColor = System.Drawing.Color.White;
+            this.btnMerge.Location = new System.Drawing.Point(0, 150);
+            this.btnMerge.Name = "btnMerge";
+            this.btnMerge.Size = new System.Drawing.Size(198, 50);
+            this.btnMerge.TabIndex = 3;
+            this.btnMerge.Text = "Merge";
+            this.btnMerge.TextColor = System.Drawing.Color.White;
+            this.btnMerge.UseVisualStyleBackColor = false;
+            this.btnMerge.Click += new System.EventHandler(this.btnMerge_Click);
+            // 
+            // iconPictureBox1
+            // 
+            this.iconPictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(115)))), ((int)(((byte)(156)))), ((int)(((byte)(158)))));
+            this.iconPictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.iconPictureBox1.IconChar = FontAwesome.Sharp.IconChar.Times;
+            this.iconPictureBox1.IconColor = System.Drawing.Color.White;
+            this.iconPictureBox1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconPictureBox1.IconSize = 29;
+            this.iconPictureBox1.Location = new System.Drawing.Point(765, 5);
+            this.iconPictureBox1.Name = "iconPictureBox1";
+            this.iconPictureBox1.Size = new System.Drawing.Size(32, 29);
+            this.iconPictureBox1.TabIndex = 1;
+            this.iconPictureBox1.TabStop = false;
+            this.iconPictureBox1.Click += new System.EventHandler(this.iconPictureBox1_Click);
+            // 
+            // iconPictureBox2
+            // 
+            this.iconPictureBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(115)))), ((int)(((byte)(156)))), ((int)(((byte)(158)))));
+            this.iconPictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.iconPictureBox2.IconChar = FontAwesome.Sharp.IconChar.Minus;
+            this.iconPictureBox2.IconColor = System.Drawing.Color.White;
+            this.iconPictureBox2.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconPictureBox2.IconSize = 29;
+            this.iconPictureBox2.Location = new System.Drawing.Point(727, 5);
+            this.iconPictureBox2.Name = "iconPictureBox2";
+            this.iconPictureBox2.Size = new System.Drawing.Size(32, 29);
+            this.iconPictureBox2.TabIndex = 2;
+            this.iconPictureBox2.TabStop = false;
+            this.iconPictureBox2.Click += new System.EventHandler(this.iconPictureBox2_Click);
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(11, 387);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(35, 13);
+            this.labelStatus.TabIndex = 4;
+            this.labelStatus.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelChild);
+            this.Controls.Add(this.panelBotones);
+            this.Controls.Add(this.panelSuperior);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelBotones.ResumeLayout(false);
+            this.panelBotones.PerformLayout();
+            this.panelSuperior.ResumeLayout(false);
+            this.panelSuperior.PerformLayout();
+            this.panelChild.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelBotones;
+        private RJCodeAdvance.RJControls.RJButton btnMerge;
+        private RJCodeAdvance.RJControls.RJButton btnSelect;
+        private RJCodeAdvance.RJControls.RJButton btnInsert;
+        private RJCodeAdvance.RJControls.RJButton btnBubble;
+        private System.Windows.Forms.Panel panelSuperior;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panelChild;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private FontAwesome.Sharp.IconPictureBox iconPictureBox2;
+        private FontAwesome.Sharp.IconPictureBox iconPictureBox1;
+        private System.Windows.Forms.Label labelStatus;
+    }
+}
+
---
+++ SortingVisualizer/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SortingVisualizer
+{
+    public partial class Form1 : Form
+    {
+        int amount = 20;
+        int[] dt = new int[20];
+        Random rand = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < amount; i++)
+                dt[i] = rand.Next();
+            chart1.Series[0].Points.DataBindY(dt);
+        }
+
+        private void bubbleSort()
+        {
+            int aux;
+            bool moved; //ESTA VARIABLE NOS SIRVE POR SI HAY QUE HACER VARIOS ACOMODAMIENTOS DEL MISMO VALOR
+            do{
+                moved = false;
+                for (int i = 0; i < dt.Length - 1; i++) 
+                    if (dt[i] > dt[i + 1]){ //COMPARA CADA VALOR CON SU VECINO PARA VER SI ESTA EN ORDEN CORRECTO
+                        aux = dt[i + 1]; //SI ESTA DESORDENADO INTERCAMBIA LOS VALORES
+                        dt[i + 1] = dt[i];
+                        dt[i] = aux;
+                        moved = true;
+                        graphUpdate();
+                    }
+            } while (moved);
+        }
+
+        private void insertionSort()
+        {
+            int item, current;
+            for (int i = 0; i < dt.Length; i++){ //LOOP DE TODOS LOS ITEMS
+                item = dt[i];
+                current = i;
+                while(current > 0 && dt[current -1] > item){ //LOOP DE LOS ITEMS INFERIORES AL ACTUAL
+                    dt[current] = dt[current - 1];
+                    current--;
+                    graphUpdate();
+                }
+                dt[current] = item;
+            }
+        }
+
+        private void selectionSort()
+        {
+            int min, aux;
+            for (int i = 0; i < dt.Length; i++){
+                min = i;
+                for (int j = i + 1; j < dt.Length; j++)
+                    if (dt[min] > dt[j])
+                        min = j;
+                if(min != i){
+                    aux = dt[min];
+                    dt[min] = dt[i];
+                    dt[i] = aux;
+                }
+                graphUpdate();
+            }
+        }
+
+        public void MainMerge(int[] numbers, int left, int mid, int right)
+        {
+            int[] temp = new int[25];
+            int i, eol, num, pos;
+            eol = (mid - 1);
+            pos = left;
+            num = (right - left + 1);
+
+            while ((left <= eol) && (mid <= right))
+            {
+                if (numbers[left] <= numbers[mid])
+                    temp[pos++] = numbers[left++];
+                else
+                    temp[pos++] = numbers[mid++];
+            }
+            while (left <= eol)
+                temp[pos++] = numbers[left++];
+            while (mid <= right)
+                temp[pos++] = numbers[mid++];
+            for (i = 0; i < num; i++)
+            {
+                numbers[right] = temp[right];
+                right--;
+            }
+            graphUpdate();
+        }
+
+        public void SortMerge(int[] numbers, int left, int right)
+        {
+            int mid;
+            if (right > left)
+            {
+                mid = (right + left) / 2;
+                SortMerge(numbers, left, mid);
+                SortMerge(numbers, (mid + 1), right);
+                MainMerge(numbers, left, (mid + 1), right);
+            }
+        }
+
+        private void graphUpdate()
+        {
+            chart1.Series[0].Points.DataBindY(dt);
+            chart1.Update();
+            System.Threading.Thread.Sleep(5000 / dt.Length); //DELAY PARA QUE SEA MAS FACIL DE VISUALIZAR
+        }
+
+        private void btnBubble_Click(object sender, EventArgs e)
+        {
+            bubbleSort();
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            insertionSort();
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            selectionSort();
+        }
+
+        private void btnMerge_Click(object sender, EventArgs e)
+        {
+            SortMerge(dt, 0, dt.Length - 1);
+        }
+
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+        private void panelSuperior_MouseDown_1(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void iconPictureBox2_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void iconPictureBox1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
